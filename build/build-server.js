d3fe186dee872df7ce05aca9d9696be18275eba7
liushida <laushida@outlook.com>
2018-07-27 11:41:30 +0800

    refactor build file

--- build/build-server.js
+++ build/build-server.js
@@ -1,41 +1,6 @@
-const fs = require('fs');
-const promisify = require('util').promisify;
-const path = require('path');
-const glob = require('glob');
-const signale = require('signale');
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
+const signale = require('signale')
+const changeHtmlSrc = require('./util')
 
-const files = glob.sync(path.resolve(__dirname, '../dist/**/*.html'))
-
-const changeHtmlSrc = async (files) => {
-  const filesData = {}
-
-  for (let file of files) {
-    const buffer = await readFile(file)
-    filesData[file] = buffer.toString()
-  }
-
-  for (let file in filesData) {
-    const insertPath = findFileDeepStr(file)
-    filesData[file] = filesData[file].replace(/ src="(..\/){0,}(?!http)/g, ` src="${insertPath}`)
-    filesData[file] = filesData[file].replace(/ href="(..\/){0,}(?!http)/g, ` href="${insertPath}`)
-    await writeFile(file, filesData[file])
-  }
-
-}
-
-function findFileDeepStr (path) {
-  const startIndex = path.indexOf('dist/')
-  const dirs = path.slice(startIndex).split('/')
-  const deep = dirs.length - 2
-  let result = ''
-  for (let i = 0; i < deep; i++) {
-    result += '../'
-  }
-  return result
-}
-
-changeHtmlSrc(files).then(() => {
-  signale.success('生产环境构建完成');
+changeHtmlSrc().then(() => {
+  signale.success('生产环境构建完成')
 })
--- build/dev-server.js
+++ build/dev-server.js
@@ -1,56 +1,21 @@
-const fs = require('fs');
-const promisify = require('util').promisify;
-const path = require('path');
-const glob = require('glob');
-const express = require('express');
-const parcel = require('parcel-bundler');
-const signale = require('signale');
-const app = express();
-
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
+const express = require('express')
+const parcel = require('parcel-bundler')
+const signale = require('signale')
+const app = express()
+const changeHtmlSrc = require('./util')
 
 const bundler = new parcel('src/index.html', {
   publicUrl: './',
   hmr: false,
   cache: true,
   sourceMaps: false
-});
+})
 
-app.use(bundler.middleware());
+app.use(bundler.middleware())
 
 bundler.on('bundled', async () => {
-  const files = glob.sync(path.resolve(__dirname, '../dist/**/*.html'))
-  await changeHtmlSrc(files)
-  signale.success('success');
-});
+  await changeHtmlSrc()
+  signale.success('success')
+})
 
-const changeHtmlSrc = async (files) => {
-  const filesData = {}
-
-  for (let file of files) {
-    const buffer = await readFile(file)
-    filesData[file] = buffer.toString()
-  }
-
-  for (let file in filesData) {
-    const insertPath = findFileDeepStr(file)
-    filesData[file] = filesData[file].replace(/ src="(..\/){0,}(?!http)/g, ` src="${insertPath}`)
-    filesData[file] = filesData[file].replace(/ href="(..\/){0,}(?!http)/g, ` href="${insertPath}`)
-    await writeFile(file, filesData[file])
-  }
-
-}
-
-function findFileDeepStr (path) {
-  const startIndex = path.indexOf('dist/')
-  const dirs = path.slice(startIndex).split('/')
-  const deep = dirs.length - 2
-  let result = ''
-  for (let i = 0; i < deep; i++) {
-    result += '../'
-  }
-  return result
-}
-
-app.listen(1234);
+app.listen(1234)
